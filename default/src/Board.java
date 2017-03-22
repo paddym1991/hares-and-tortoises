b7d188cc39e2f55b7e5a4afa57bac77122667b8b
cgahan <cathalmgahan@gmail.com>
2017-03-22 15:47:39 +0000

    Basic Board properties and constructor

--- default/src/Board.java
+++ default/src/Board.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
-    //Placeholder
+    private List<Player> players;
+    private List<Square> squares;
+
+    public Board() {
+        players = new ArrayList<>();
+        squares = new ArrayList<>();
+    }
 }
