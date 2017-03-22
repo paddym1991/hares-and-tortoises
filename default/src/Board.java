e180270943f039eda132db6fac22df6196d60a43
cgahan <cathalmgahan@gmail.com>
2017-03-22 16:15:21 +0000

    Slightly more for Board

--- default/src/Board.java
+++ default/src/Board.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Board {
@@ -6,7 +7,18 @@ public class Board {
     private List<Square> squares;
 
     public Board() {
-        players = new ArrayList<>();
+        players = Arrays.asList(new Player());
+        squares = Arrays.asList(
+                new Square(),   //Start square
+                new Square(),   //Square 1
+                new Square(),   //Square 2
+                new Square(),   //Square 3
+                new Square()    //Square 4
+        );
+    }
+
+    public Board(int numPlayers) {
+        //TODO: Fill "players" with the right number of Player instances
         squares = new ArrayList<>();
     }
 }
