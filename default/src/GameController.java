529943a35a52bd320b3becd32c32faac6e5102ae
Paddym1991 <paddym1991.pm@gmail.com>
2017-03-23 11:01:36 +0000

    Beginnings of GameController

--- default/src/GameController.java
+++ default/src/GameController.java
@@ -1,5 +1,29 @@
 /**
  * Created by admin on 22/03/2017.
  */
+
+
+import java.util.Scanner;
+
 public class GameController {
+
+    private Scanner input;
+    private Player player;
+    //private Board board;
+    // private Square square;
+
+    public static void main(String[] args) {
+        new GameController();
+    }
+
+    public GameController() {
+        input = new Scanner(System.in);
+
+        System.out.println("Welcome to Hare and Tortoise");
+        System.out.println("----------------------------");
+        System.out.print("\nPlease enter number of players: ");
+        int numOfPlayers = input.nextInt();
+    }
+
+
 }
