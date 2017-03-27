4b911a709381d27ae5f8a03a9d9c5a3a84bf4ad9
cgahan <cathalmgahan@gmail.com>
2017-03-27 17:46:44 +0100

    Implemented LettuceSquare class. Added placeholder methods to Player and Square for functionality that LettuceSquare needs to use; these can be implemented later.

--- default/src/Player.java
+++ default/src/Player.java
@@ -1,28 +1,61 @@
 public class Player {
-	private int boardPosition = 0;
-	private Board board;
+    private int boardPosition = 0;
+    private Board board;
+    private boolean faceDown;
 
-	public Player(Board board) {
-		this.board = board;
-	}
+    public Player(Board board) {
+        this.board = board;
+    }
 
-	public int getBoardPosition() {
-		return boardPosition;
-	}
+    public int getBoardPosition() {
+        return boardPosition;
+    }
 
-	public void movePlayer(int numberOfSquares) {
-		boardPosition += numberOfSquares;
-	}
+    public void movePlayer(int numberOfSquares) {
+        boardPosition += numberOfSquares;
+    }
 
-	public boolean atEnd() {
+    public boolean atEnd() {
 
-		return board.atEnd(getBoardPosition()); // the board knows the rules for
-												// being at the end of the
-												// board.
-												// so ask board are we at the
-												// end (.atEnd) by giving it our
-												// board position
-												// (getBoardPosition).
-	}
+        return board.atEnd( getBoardPosition() ); //the board knows the rules for being at the end of the board.
+        //so ask board are we at the end (.atEnd) by giving it our board position
+        //(getBoardPosition).
+    }
 
+    public void addCarrots(int amount) {
+        //TODO
+    }
+    public void takeCarrots(int amount) {
+        //TODO
+    }
+
+    public int getLettuce() {
+        return 0; //TODO: Get actual result
+    }
+    public void takeLettuce() {
+        //TODO: Reduce the player's stock of lettuce by one. No way to gain extra lettuce, an so addLettuce() method isn't needed.
+    }
+
+    public String getName() {
+        return "Placeholder"; //TODO: Get player's name (e.g. "Player 1")
+    }
+
+    public int getRacePosition() {
+        return 0; //TODO: Get position in race relative to other players (1st, 2nd, etc.)
+    }
+
+    public void endTurn() {
+        //TODO: Called whenever an effect would end the player's turn without allowing them to move
+    }
+
+    //Used by Lettuce squares
+    public boolean isFaceDown() {
+        return faceDown;
+    }
+    public void flipFaceDown() {
+        faceDown = true;
+    }
+    public void flipFaceUp() {
+        faceDown = false;
+    }
 }
--- default/src/Square.java
+++ default/src/Square.java
@@ -1,6 +1,19 @@
-/**
- * Created by admin on 27/03/2017.
- */
 public class Square {
+
+    public Square() {
+        //
+    }
+
+    public boolean canLandOn(Player player) {
+        return true; //TODO: Check if it's possible to land on this square
+    }
+
+    public void onLandOn(Player player) {
+        //Do nothing
+    }
+
+    public void onTurnStart(Player player) {
+        //Do nothing
+    }
 
 }
